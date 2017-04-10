c15fbe2f06f7e663062042e3939ae28d11d1a0a3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-10 14:07:02 +0600

    jivosite enabled if PARAM_JIVOSITE_WIDGET_ID is set

--- web/__jivosite.php
+++ web/__jivosite.php
@@ -1,8 +1,11 @@
-<?php if($_SERVER['HTTP_HOST'] != 'eurasia.loc'): ?>
+<?php 
+require_once './.settings.php';
+if (defined('PARAM_JIVOSITE_WIDGET_ID')):
+?>
 
 <!-- BEGIN JIVOSITE CODE {literal} -->
 <script type="text/javascript">
-(function(){ var widget_id = 'ca5q1XiaDl';var d=document;var w=window;function l(){
+(function(){ var widget_id = '<?=PARAM_JIVOSITE_WIDGET_ID?>';var d=document;var w=window;function l(){
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/geo-widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
 <!-- {/literal} END JIVOSITE CODE -->
 
