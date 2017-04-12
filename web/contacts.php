7509bdd062cf2f97fa7d09611572e52728122f8e
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-12 17:10:26 +0600

    YM GA contacts

--- web/contacts.php
+++ web/contacts.php
@@ -55,45 +55,8 @@ $offices = json_decode($offices, true);
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-        <?php if($_SERVER['HTTP_HOST'] != 'eurasia.loc'): ?>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-84812378-1', 'auto');
-            ga('send', 'pageview');
-
-        </script>
-
-        <script type="text/javascript">
-            (function (d, w, c) {
-                (w[c] = w[c] || []).push(function() {
-                    try {
-                        w.yaCounter39882375 = new Ya.Metrika({
-                            id:39882375,
-                            clickmap:true,
-                            trackLinks:true,
-                            accurateTrackBounce:true,
-                            webvisor:true
-                        });
-                    } catch(e) { }
-                });
-
-                var n = d.getElementsByTagName("script")[0],
-                    s = d.createElement("script"),
-                    f = function () { n.parentNode.insertBefore(s, n); };
-                s.type = "text/javascript";
-                s.async = true;
-                s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-                if (w.opera == "[object Opera]") {
-                    d.addEventListener("DOMContentLoaded", f, false);
-                } else { f(); }
-            })(document, window, "yandex_metrika_callbacks");
-        </script>
-        <?php endif; ?>
+        <?php include './__GA.php'; ?>
+        <?php include './__YM.php'; ?>
     </head>
 
     <body>
