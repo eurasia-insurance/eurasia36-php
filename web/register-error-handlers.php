9e817ed3953d692451ae6d1d62431ccf1747fbf2
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-25 13:37:15 +0600

    error handling

---
+++ web/register-error-handlers.php
@@ -0,0 +1,24 @@
+<?
+set_exception_handler('myExceptionHandler');
+set_error_handler('myErrorHandler');
+
+function to500() {
+    ob_end_clean(); # try to purge content sent so far
+    http_response_code(500);
+#    header("Location: /500.html");
+    include './500.html';
+    die;
+}
+
+function myExceptionHandler($ex) {
+    error_log("Uncaught exception class=" . get_class($ex) . " message=" . $ex->getMessage() . " line=" . $ex->getLine());
+    to500();
+    die;
+}
+
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+    error_log("Fatal error errno=" . $errno . " errstr=" . $errstr . " errfile=" . $errfile . " errline=" . $errline);
+    to500();
+    die;
+}
+?>
