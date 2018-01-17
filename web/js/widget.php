73b12eb17288bec09924740bc7934139ea74fbd6
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-17 20:06:23 +0600

    no-cache

--- web/js/widget.php
+++ web/js/widget.php
@@ -1,6 +1,9 @@
 <?
 require_once './../.settings.php';
 header('Content-Type: application/javascript');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 ?>
 var host = '<?=$_SERVER['REQUEST_SCHEME']?>://<?=$_SERVER['HTTP_HOST']?>:<?=$_SERVER['SERVER_PORT']?>/';
 
