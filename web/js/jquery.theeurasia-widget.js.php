80bc9999de03fdf1d0927834edaafc20c8b50b9c
iPasha <pavel@grafica.kz>
2018-02-08 17:53:23 +0600

    виджет для корп. сайта #47

---
+++ web/js/jquery.theeurasia-widget.js.php
@@ -0,0 +1,33 @@
+/*
+<?php
+
+require_once(__DIR__ . '/../api/EurasiaAPI.php');
+
+header('Content-Type: application/javascript');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$host = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
+if ( ($_SERVER['REQUEST_SCHEME'] == 'https' && $_SERVER['SERVER_PORT'] != '443') || ($_SERVER['REQUEST_SCHEME'] == 'http' && $_SERVER['SERVER_PORT'] != '80')) {
+    $host .= ':' . $_SERVER['SERVER_PORT'];
+}
+?>
+*/
+var host = '<?= $host ?>';
+<?php
+
+$ping = EurasiaAPI::ping();
+if($ping != '0') {
+    echo "var showPhones = true;\n";
+}
+
+
+if(defined('PARAM_SHOW_EUBANK_BONUS') && PARAM_SHOW_EUBANK_BONUS == 1) {
+    echo "var showBonus = 1;\n";
+} else {
+    echo "var showBonus = 0;\n";
+}
+
+
+include_once 'jquery.theeurasia-widget.js';
